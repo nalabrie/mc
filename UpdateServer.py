4899f762ca8dbdae2d62514fe68e5c9b4a53dc2f
nalabrie <ku9ca7cmlk@gmail.com>
2024-08-18 12:29:39 -0400

    built out more of the skeleton, but still not operational. Stopping here before experimenting with 2 different ways forward.

--- UpdateServer.py
+++ UpdateServer.py
@@ -1,1 +1,6 @@
+#!/usr/bin/env python
+
+# Description:  This script is used to update a Minecraft server to the latest release.
+#       Usage:  ./UpdateServer.py <server directory>
+
 # TODO: transfer old update script here and adjust as needed
--- start.sh
+++ start.sh
@@ -9,11 +9,11 @@ set -euo pipefail
 # ***      DO NOT CHANGE ANYTHING ELSE       ***
 # **********************************************
 
-SERVER_ROOT="$HOME/srv"     # root directory for all servers
-SERVER_NAME="chubharbor"    # server name (server directory name)
-RAM=10                      # server RAM (in GB)
-LOCAL_BACKUP_DIR="DISABLE"  # local backup directory (set to DISABLE to disable local backups)
-REMOTE_BACKUP_DIR="DISABLE" # remote backup directory (set to DISABLE to disable remote backups)
+SERVER_ROOT="$HOME/.local/srv" # root directory for all servers
+SERVER_NAME="chubharbor"       # server name (server directory name)
+RAM=10                         # server RAM (in GB)
+LOCAL_BACKUP_DIR="DISABLE"     # local backup directory (set to DISABLE to disable local backups)
+REMOTE_BACKUP_DIR="DISABLE"    # remote backup directory (set to DISABLE to disable remote backups)
 
 # **********************************************
 # *** DO NOT CHANGE ANYTHING BELOW THIS LINE ***
@@ -41,8 +41,20 @@ pip install --upgrade -r requirements.txt
 UPDATE_SERVER_SCRIPT_PATH="$(pwd)/UpdateServer.py"  # updates the server
 LOCAL_BACKUP_SCRIPT_PATH="$(pwd)/local_backup.sh"   # local server backup script
 REMOTE_BACKUP_SCRIPT_PATH="$(pwd)/remote_backup.sh" # remote server backup script
+RUN_SERVER_SCRIPT_PATH="$(pwd)/run_server.sh"       # runs the server
+MANAGER_SCRIPT_PATH="$(pwd)/manager.py"             # server manager script
 
-#? === MAIN ===
+# ensure all paths exist
+# if they are not executable, make them executable
+for path in "$UPDATE_SERVER_SCRIPT_PATH" "$LOCAL_BACKUP_SCRIPT_PATH" "$REMOTE_BACKUP_SCRIPT_PATH" "$RUN_SERVER_SCRIPT_PATH" "$MANAGER_SCRIPT_PATH"; do
+    if [ ! -f "$path" ]; then
+        echo "Error: $path does not exist."
+        exit 1
+    fi
+done
+chmod +x "$UPDATE_SERVER_SCRIPT_PATH" "$LOCAL_BACKUP_SCRIPT_PATH" "$REMOTE_BACKUP_SCRIPT_PATH" "$RUN_SERVER_SCRIPT_PATH" "$MANAGER_SCRIPT_PATH"
+
+#? === UPDATE & START SERVER ===
 
 # cd to server directory
 cd "$SERVER_ROOT/$SERVER_NAME"
@@ -50,3 +62,17 @@ cd "$SERVER_ROOT/$SERVER_NAME"
 # make tmux sessions for the server and the server manager
 tmux new-session -d -s "$SERVER_NAME"
 tmux new-session -d -s "${SERVER_NAME}_manager"
+
+# update the server
+# run the server if the update is successful
+if "$UPDATE_SERVER_SCRIPT_PATH" "$SERVER_ROOT/$SERVER_NAME"; then
+    tmux send-keys -t "$SERVER_NAME" "\"$RUN_SERVER_SCRIPT_PATH\" $RAM" Enter
+else
+    echo "Failed to update the server. Server not started."
+    exit 1
+fi
+
+# ? === BACKUPS ===
+
+# start the server manager
+# TODO: unsure if I want to do it the "Windows way" or move everything to bash scripts & cron jobs
